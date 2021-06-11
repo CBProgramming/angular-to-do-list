700752deb8f222311f32371754805c4a606f954c
ChrisBurrellT7145969 <t7145969@live.tees.ac.uk>
2021-06-11 12:39:14 +0100

    Delete Todo on UI and server implemented

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
 
 @Component({
@@ -9,6 +9,7 @@ import { Todo } from 'src/app/models/Todo';
 export class TodoItemComponent implements OnInit {
 
   @Input() todo: Todo = { id: 1, title: "wrong", completed:false};
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
 
   constructor() { }
 
@@ -29,6 +30,6 @@ export class TodoItemComponent implements OnInit {
   }
 
   onDelete(todo: Todo) {
-    console.log('delete');
+    this.deleteTodo.emit(todo);
   }
 }
